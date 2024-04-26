df0b376cc94358246f7e58582c46ef579845a124
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-04-26 17:44:38 +0200

    Integrated API
    Made function for updating current weather work.

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,7 @@ import ShowCurrentParameters from "./ShowCurrentParameters";
 import Header from "./Header";
 import WeatherForecast from "./WeatherForecast";
 import Footer from "./Footer";
+import axios from "axios";
 
 export default function Form() {
   const [city, setCity] = useState(null);
@@ -12,7 +13,10 @@ export default function Form() {
     <div className="card weather-today">
       <div className="card-body body-weather-today">
         <div className="row">
-          <ShowCurrentTemperature value={20} />
+          <ShowCurrentTemperature
+            value={20}
+            img="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+          />
           <ShowCityDateTime city="Berlin" day="Thursday" time="15:59" />
           <ShowCurrentParameters
             description="Few clouds"
@@ -27,21 +31,34 @@ export default function Form() {
   function handleSubmit(event) {
     event.preventDefault();
     if (city) {
-      setWeatherToday(
-        <div className="card weather-today">
-          <div className="card-body body-weather-today">
-            <div className="row">
-              <ShowCurrentTemperature value={20} />
-              <ShowCityDateTime city={city} day="Thursday" time="15:59" />
-              <ShowCurrentParameters
-                description="Few clouds"
-                humidity={61}
-                windSpeed={7}
-              />
+      function getData(response) {
+        const temp = Math.round(response.data.temperature.current);
+        const description = response.data.condition.description;
+        const humidity = response.data.temperature.humidity;
+        const wind = Math.round(response.data.wind.speed);
+
+        setWeatherToday(
+          <div className="card weather-today">
+            <div className="card-body body-weather-today">
+              <div className="row">
+                <ShowCurrentTemperature
+                  value={temp}
+                  img={response.data.condition.icon_url}
+                />
+                <ShowCityDateTime city={city} day="Thursday" time="15:59" />
+                <ShowCurrentParameters
+                  description={description}
+                  humidity={humidity}
+                  windSpeed={wind}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }
+      const apiKey = `9fb66eat3c45068of64821d7cabe200f`;
+      const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(getData);
     }
   }
 
--- src/ShowCurrentTemperature.js
+++ src/ShowCurrentTemperature.js
@@ -3,11 +3,7 @@ import React from "react";
 export default function ShowCurrentTemperature(props) {
   return (
     <div className="col weather-today-col">
-      <img
-        src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-        alt="Weather icon"
-        className="weather-icon"
-      />
+      <img src={props.img} alt="Weather icon" className="weather-icon" />
       <div className="temperature-today">{props.value}</div>
       <span className="temp-unit">°C</span>
     </div>
