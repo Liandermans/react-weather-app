340201e14000d04fb20cf6ec1eb3dca588556032
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-04-26 16:24:06 +0200

    Updated links

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <a
-        href="https://github.com/Liandermans/SheCodes-Weather-App"
+        href="https://github.com/Liandermans/react-weather-app"
         target="_blank"
         rel="noreferrer"
       >
@@ -13,7 +13,11 @@ export default function Footer() {
       by Lianne de Winter
       <br />
       Hosted on{" "}
-      <a href="https://www.netlify.com/" target="_blank" rel="noreferrer">
+      <a
+        href="https://stalwart-squirrel-4598c1.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
         Netlify
       </a>
     </footer>
