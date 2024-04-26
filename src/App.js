457618497e28f1434f0935f7b423228a9030b948
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-04-26 17:10:45 +0200

    Made function for updating city work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import Header from "./Header";
 import Form from "./Form";
-import WeatherToday from "./WeatherToday";
-import WeatherForecast from "./WeatherForecast";
-import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="Weather">
-        <Header />
-        <Form />
-        <WeatherToday />
-        <WeatherForecast />
-        <Footer />
-      </div>
+      <Form />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import ShowCityDateTime from "./ShowCityDateTime";
+import ShowCurrentTemperature from "./ShowCurrentTemperature";
+import ShowCurrentParameters from "./ShowCurrentParameters";
+import Header from "./Header";
+import WeatherForecast from "./WeatherForecast";
+import Footer from "./Footer";
 
 export default function Form() {
+  const [city, setCity] = useState(null);
+  const [weatherToday, setWeatherToday] = useState(
+    <div className="card weather-today">
+      <div className="card-body body-weather-today">
+        <div className="row">
+          <ShowCurrentTemperature value={20} />
+          <ShowCityDateTime city="Berlin" day="Thursday" time="15:59" />
+          <ShowCurrentParameters
+            description="Few clouds"
+            humidity={61}
+            windSpeed={7}
+          />
+        </div>
+      </div>
+    </div>
+  );
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city) {
+      setWeatherToday(
+        <div className="card weather-today">
+          <div className="card-body body-weather-today">
+            <div className="row">
+              <ShowCurrentTemperature value={20} />
+              <ShowCityDateTime city={city} day="Thursday" time="15:59" />
+              <ShowCurrentParameters
+                description="Few clouds"
+                humidity={61}
+                windSpeed={7}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  function getCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
-    <form id="search-form">
-      <input
-        className="entry-field"
-        type="text"
-        placeholder="Enter city"
-        id="entered-value"
-        autoFocus
-        autoComplete="off"
-      />
-      <i
-        className="fa-solid fa-location-dot location-icon"
-        id="get-location"
-      ></i>
-      <br />
-      <input
-        className="submit-button"
-        id="submit-button"
-        type="submit"
-        value="Submit"
-      />
-    </form>
+    <div className="Weather">
+      <Header />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="entry-field"
+          type="text"
+          placeholder="Enter city"
+          autoFocus
+          autoComplete="off"
+          onChange={getCity}
+        />
+        <i className="fa-solid fa-location-dot location-icon"></i>
+        <br />
+        <input className="submit-button" type="submit" value="Submit" />
+      </form>
+      {weatherToday}
+      <WeatherForecast />
+      <Footer />
+    </div>
   );
 }
